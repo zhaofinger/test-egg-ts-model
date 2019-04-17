7b9171b9cc1a02e1aec86bc90482a5f05cd38830
zhaofinger <zhaohaibo1994@gmail.com>
2019-04-17 18:09:18 +0800

    [fix] 添加错误操作

--- app/service/User.ts
+++ app/service/User.ts
@@ -3,9 +3,9 @@ import { Service } from 'egg';
 /**
  * Test Service
  */
-export default class Test extends Service {
+export default class User extends Service {
   public async list() {
     const { ctx } = this;
-    ctx.model.User.all();
+    ctx.model.User.create({ id: 1 });
   }
 }
